81c6e91b32de38dad1554d5ecfbf5fbfb429be0b
kapiotrow <kapiotrow@student.agh.edu.pl>
2024-01-20 22:42:41 +0100

    TSOP and TSAL done

--- TSAL/src/main.cpp
+++ TSAL/src/main.cpp
@@ -7,24 +7,26 @@ uint32_t delayCounter = 0;
 void setup() {
   Serial.begin(9600);
   pinMode(TriggerPin, INPUT_PULLUP);
+      analogWrite(10, 180);
+    TCA0.SINGLE.CTRLA = 0xFF;
 }
 
 void loop() {
-  if (digitalRead(TriggerPin) == 0 && triggerPulled == 0) {
-    triggerPulled = 1;
-    delayCounter = 0;
-    analogWrite(10, 180);
-    TCA0.SINGLE.CTRLA = 0xFF;
-    Serial.write("Trigger pulled\n");
-  }
+  // if (digitalRead(TriggerPin) == 0 && triggerPulled == 0) {
+  //   triggerPulled = 1;
+  //   delayCounter = 0;
+  //   analogWrite(10, 180);
+  //   TCA0.SINGLE.CTRLA = 0xFF;
+  //   Serial.write("Trigger pulled\n");
+  // }
 
-  delayCounter = delayCounter + 1;
-  if (delayCounter == 50) {
-    analogWrite(10, 0);
-  }
+  // delayCounter = delayCounter + 1;
+  // if (delayCounter == 50) {
+  //   analogWrite(10, 0);
+  // }
 
-  if (delayCounter >= 200000) {
-    delayCounter = 0;
-    triggerPulled = 0;
-  }
+  // if (delayCounter >= 200000) {
+  //   delayCounter = 0;
+  //   triggerPulled = 0;
+  // }
 }
