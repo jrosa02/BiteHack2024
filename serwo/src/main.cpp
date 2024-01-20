961b0cc0c1c7b8edac00db1d7f58a73aac00c258
kapiotrow <kapiotrow@student.agh.edu.pl>
2024-01-20 13:50:01 +0100

    sensor up-down movement

--- serwo/src/main.cpp
+++ serwo/src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
+#include <Stepper.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Defines the number of steps per rotation
+const int stepsPerRevolution = 2038;
+const int minSpeed = 5;
+const int maxSpeed = 20;
+
+long randSteps;
+long randSpeed;
+
+// Creates an instance of stepper class
+// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    //set seed, assuming pin0 is NOT connected
+    randomSeed(analogRead(0));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  //generate random number of steps
+  randSteps = random(-stepsPerRevolution, stepsPerRevolution);
+  // generate random speed
+  randSpeed = random(minSpeed, maxSpeed);
+	myStepper.setSpeed(randSpeed);
+	myStepper.step(randSteps);
+	delay(100);
 }
