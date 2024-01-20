69faa74329ecb6dca97a025249aed762f65a52cc
kapiotrow <kapiotrow@student.agh.edu.pl>
2024-01-21 00:15:43 +0100

    left_right movement with proximity sensors

--- silniki_DC_mostek_H/src/main.cpp
+++ silniki_DC_mostek_H/src/main.cpp
@@ -12,15 +12,94 @@
 #define IN4 6 //pin connected with motor B
 
 //pins for proximity sensor
-#define trigPin 12 //proximity sensor trigger pin
-#define echoPin 13 //proximity sensor echo pin
+#define trigPinLeft 12 //proximity sensor trigger pin
+#define trigPinRight 14
+#define echoPinLeft 13 //proximity sensor echo pin
+#define echoPinRight 15
 
-uint8_t proxSensorLeft = 0;
-uint8_t proxSensorRight = 0;
+volatile uint8_t proxSensorLeft = 0;
+volatile uint8_t proxSensorRight = 0;
+
 long currentMillis = 0;
 long prevMillis = 0;
 uint8_t chooseDirection = 3;
 long randTime = 0;
+
+
+//helper function for checking distance
+void checkDistance(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(trigPin, LOW);
+
+  long time = pulseIn(echoPin, HIGH);
+  long dist = time/58;
+
+  if (dist < 5) {
+    if (trigPin == trigPinLeft) {
+      proxSensorLeft = 1;
+    }
+    else if (trigPin == trigPinRight) {
+      proxSensorRight = 1;
+    }
+  }
+}
+
+//helper functions definitions
+//We assume that:
+//left direction -> counter clockwise
+//right directon -> clockwise
+
+void stop(uint8_t t=100){
+  currentMillis = millis();
+  analogWrite(enA, 0); 
+  analogWrite(enB, 0);
+  while(currentMillis - prevMillis >= t) {
+    currentMillis = millis();
+  }
+  prevMillis = currentMillis;
+}
+
+void goRight(uint8_t t){
+  currentMillis = millis();
+  checkDistance(trigPinRight, echoPinRight);
+  if (!proxSensorRight) {
+    analogWrite(enA, 255);  
+    analogWrite(enB, 255);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  }
+  
+  while(currentMillis - prevMillis >= t && !proxSensorRight) {
+    checkDistance(trigPinRight, echoPinRight);
+    currentMillis = millis();
+  }
+  prevMillis = currentMillis;
+  stop();
+}
+
+void goLeft(uint8_t t){
+  currentMillis = millis();
+  checkDistance(trigPinLeft, echoPinLeft);
+  if (!proxSensorLeft) {
+    analogWrite(enA, 255); 
+    analogWrite(enB, 255); 
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
+  while(currentMillis - prevMillis >= t && !proxSensorLeft) {
+    checkDistance(trigPinLeft, echoPinLeft);
+    currentMillis = millis();
+  }
+  prevMillis = currentMillis;
+  stop();
+}
 
 
 void setup() {
@@ -31,8 +110,10 @@ void setup() {
   pinMode(enB, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(trigPinRight, OUTPUT);
+  pinMode(trigPinLeft, OUTPUT);
+  pinMode(echoPinRight, INPUT);
+  pinMode(echoPinLeft, INPUT);
   randomSeed(analogRead(0));
 }
 
@@ -53,58 +134,9 @@ void loop() {
         goRight(randTime);
       }
       break;
-    default;
+    default:
       stop();
   }
 }
 
-//We assume that:
-//left direction -> counter clockwise
-//right directon -> clockwise
 
-//helper functions definitions
-
-void stop(uint8_t t=100){
-  currentMillis = millis();
-  analogWrite(enA, 0); 
-  analogWrite(enB, 0);
-  while(currentMillis - prevMillis >= t) {
-    currentMillis = millis();
-  }
-  prevMillis = currentMillis;
-}
-
-void goRight(uint8_t t){
-  currentMillis = millis();
-  if (!proxSensorRight) {
-    analogWrite(enA, 255);  
-    analogWrite(enB, 255);
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-  }
-  
-  while(currentMillis - prevMillis >= t && !proxSensorRight) {
-    currentMillis = millis();
-  }
-  prevMillis = currentMillis;
-  stop();
-}
-
-void goLeft(uint8_t t){
-  currentMillis = millis();
-  if (!proxSensorLeft) {
-    analogWrite(enA, 255); 
-    analogWrite(enB, 255); 
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
-  }
-  while(currentMillis - prevMillis >= t && !proxSensorLeft) {
-    currentMillis = millis();
-  }
-  prevMillis = currentMillis;
-  stop();
-}
