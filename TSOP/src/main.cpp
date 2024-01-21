1848d7af65361429bfda446030394c2f872d88db
kapiotrow <kapiotrow@student.agh.edu.pl>
2024-01-21 04:04:44 +0100

    aaa

--- TSOP/src/main.cpp
+++ TSOP/src/main.cpp
@@ -4,14 +4,17 @@
 
 void setup() {
   Serial.begin(9600);
-  IrReceiver.begin(12, DISABLE_LED_FEEDBACK);
-  // analogWrite(10, 180);
-  // TCA0.SINGLE.CTRLA = 0xFF;
+  // IrReceiver.begin(12, DISABLE_LED_FEEDBACK);
+  // analogWrite(10, 180);  // TCA0.SINGLE.CTRLA = 0xFF;
+  pinMode(7, INPUT_PULLUP);
 }
 
 void loop() {
-  if (IrReceiver.decode()) {
-    Serial.write("a\n");
-    IrReceiver.resume();
+  // if (IrReceiver.decode()) {
+  //   Serial.write("a\n");
+  //   IrReceiver.resume();
+  // }
+  if (digitalRead(7) == 0) {
+    Serial.write("aaa");
   }
 }
